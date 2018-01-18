ff903dfbdcdc958a699e78f667c06c3f70e376c4
李春 <lichun@500wan.com>
2018-01-18 16:15:11 +0800

    fixbug

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
  * Created by lichun on 2017/11/15.
  */
 import app from './server/index'
-app.listen(80, ()=> {
+app.listen(process.env.PORT || 8081, ()=> {
     console.log('success')
 })
---
+++ qyk.config.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lichun on 2018/1/18.
+ */
+module.exports = {
+    apps: [
+        {
+            name: 'score',
+            script: './bin/www.js',
+            kill_timeout: 3000,
+            env: {
+                NODE_ENV: 'production',
+                PORT: 8081,
+                HOST: '0.0.0.0'
+            }
+        }
+    ]
+}
