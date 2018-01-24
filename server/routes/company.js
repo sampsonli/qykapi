c1c4dc70125b7eb74cd45a867da3690450f45d87
sampsonli <740056710@qq.com>
2018-01-24 22:18:52 +0800

    fixbug

--- server/routes/company.js
+++ server/routes/company.js
@@ -10,11 +10,17 @@ router.all('/query_by_name',async (ctx) => {
     let name = ctx.query.name || ctx.request.body.name
     let pn = parseInt(ctx.query.pn || ctx.request.body.pn) || 0
     let ps = parseInt(ctx.query.ps || ctx.request.body.ps) || 20
+    let list = await company.find({name: eval(`/${name}/`)}, {name: 1, qyfddbrzs: 1, yyzz: 1}).skip(pn * ps).limit(ps)
+    if(list) {
+        list.forEach(item => {
+            item.qyfddbrzs.phoneNo = ''
+        })
+    }
     ctx.body = ctx.body = {
         code: 100,
         msg: 'ok',
         data: {
-            list: await company.find({name: eval(`/${name}/`)}, {name: 1, qyfddbrzs: 1, yyzz: 1}).skip(pn * ps).limit(ps)
+            list
         }
     }
 
@@ -26,6 +32,13 @@ router.all('/query_by_zizhi',async (ctx) => {
     let zizhi2 = ctx.query.zizhi2 || ctx.request.body.zizhi2 || /.*/
     let pn = parseInt(ctx.query.pn || ctx.request.body.pn) || 0
     let ps = parseInt(ctx.query.ps || ctx.request.body.ps) || 20
+
+    let list = await company.find({$and: [{'zizhi.name': zizhi1}, {'zizhi.name': zizhi2}]}, {name: 1, qyfddbrzs: 1, yyzz: 1}).skip(pn*ps).limit(ps)
+    if(list) {
+        list.forEach(item => {
+            item.qyfddbrzs.phoneNo = ''
+        })
+    }
     ctx.body = {
         code: 100,
         msg: 'ok',
@@ -41,11 +54,15 @@ router.all('/query_by_zizhi',async (ctx) => {
 router.all('/query_by_cid',async (ctx) => {
     let cid = ctx.query.cid || ctx.request.body.cid
     let res = await company.find({_id: cid})
+    let data = res[0]
+    data.lxrxx.mphone = data.lxrxx.mphone.replace(/(.*)(\d{4})(\d{3})$/, ($0, $1, $2, $3) => $1 + '****' + $3)
+    data.lxrxx.phone = data.lxrxx.phone.replace(/(.*)(\d{4})(\d{3})$/, ($0, $1, $2, $3) => $1 + '****' + $3)
+
     if(res && res.length) {
         ctx.body = {
             code: 100,
             msg: 'ok',
-            data: res[0]
+            data
         }
     }else {
         ctx.body = {
